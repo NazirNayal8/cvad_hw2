04e01e0c31a25473610b434208619fc1659e9994
Nazir Nayal <nnayal17@ku.edu.tr>
2022-05-06 18:33:32 +0300

    add incomplete code

--- GNNs.py
+++ GNNs.py
@@ -9,7 +9,8 @@ import utils
 class Global_Graph(nn.Module):
     def __init__(self, hidden_size):
         super().__init__()
-        pass
+
+        self.hidden_size = hidden_size
 
     def forward(self, hidden_states, attention_mask=None, mapping=None):
         pass
@@ -18,6 +19,17 @@ class Global_Graph(nn.Module):
 class Sub_Graph(nn.Module):
     def __init__(self, hidden_size, depth=3):
         super(Sub_Graph, self).__init__()
+
+        self.hidden_size = hidden_size
+        self.depth = depth
+        
+        # a single layer MLP shared for all nodes, followed by layer normalization and ReLU
+        self.g_enc = 
+        # max pooling operation
+        self.agg = 
+        # a simple concatenation
+        self.rel = 
+
 
     def forward(self, hidden_states, lengths):
         pass
